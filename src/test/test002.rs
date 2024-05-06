dda029246a17f0ab0f12c3976dbb8817442b820f
createjin <createsejin@gmail.com>
2024-05-06 21:38:16 +0900

    print_rows test passed

--- src/test/test002.rs
+++ src/test/test002.rs
@@ -28,6 +28,10 @@ fn read_cdd_data(path: &str) -> Vec<(u32, u8, String, String, String, String)> {
     let row = (0, argn, arg1, arg2, arg3, arg4);
     rows.push(row);
   }
+  rows
+}
+
+fn _sort_rows(rows: &mut [(u32, u8, String, String, String, String)]) {
   // sort order: arg1, argn, arg2, arg3
   rows.sort_by(|a, b| {
     let arg1_cmp = a.2.cmp(&b.2);
@@ -47,13 +51,26 @@ fn read_cdd_data(path: &str) -> Vec<(u32, u8, String, String, String, String)> {
       arg1_cmp // first
     }
   });
-  rows
+}
+
+fn _set_row_id(rows: &mut [(u32, u8, String, String, String, String)]) {
+  let mut i: u32 = 1;
+  for row in rows {
+    row.0 = i;
+    i += 1;
+  }
 }
 
 fn _print_rows(rows: &[(u32, u8, String, String, String, String)]) {
+  let max_id = rows.len();
+  let max_id_str = max_id.to_string();
+  let max_id_digit_num = max_id_str.len();
   for row in rows {
     let (id, argn, arg1, arg2, arg3, arg4) = row;
-    let mut row_str = format!("{id}|{argn}|{arg1}|{arg2}");
+    let mut row_str = format!(
+      "{0:>1$}|{2}|{3}|{4}",
+      id, max_id_digit_num, argn, arg1, arg2
+    );
     if *argn >= 3 {
       row_str.push('|');
       row_str.push_str(arg3);
@@ -68,6 +85,8 @@ fn _print_rows(rows: &[(u32, u8, String, String, String, String)]) {
 
 #[test]
 fn _test010() {
-  let rows = read_cdd_data("cdd_data.txt");
+  let mut rows = read_cdd_data("cdd_data.txt");
+  _sort_rows(&mut rows);
+  _set_row_id(&mut rows);
   _print_rows(&rows);
 }
